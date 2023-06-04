728154e67800f9ed15616ac175d79de66dab00bc
sieginglion <sieginglion@gmail.com>
2023-06-04 14:16:53 +0800

    pack as a cli

--- iptdump/main.py
+++ iptdump/main.py
@@ -13,6 +13,28 @@ from typing_extensions import Annotated
 app = Typer(add_completion=False)
 
 
+def run_cmd(cmd: str):
+    r = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if r.returncode != 0 or r.stderr:
+        print(f'[red]{r.stderr.decode()}', file=sys.stderr)
+        sys.exit(r.returncode)
+    return r
+
+
+def clear_traces(signum: Union[int, None] = None, frame: Union[FrameType, None] = None):
+    for chain in ['PREROUTING', 'OUTPUT']:
+        run_cmd(
+            f'iptables -t raw -L {chain} --line-numbers | grep TRACE | awk \'{{ print $1 }}\' | sort -r | xargs -I {{}} iptables -t raw -D {chain} {{}}'
+        )
+    if signum:
+        sys.exit()
+
+
+def add_traces(match_rule: str):
+    for chain in ['PREROUTING', 'OUTPUT']:
+        run_cmd(f'iptables -t raw -I {chain} 1 {match_rule} -j TRACE')
+
+
 @dataclass
 class Trace:
     IN: str
@@ -25,30 +47,8 @@ class Trace:
 
     def __post_init__(self):
         table, chain, _, index = self.REF.split(':')
-        r = run(f'iptables -t {table} -nL {chain} {index}')
+        r = run_cmd(f'iptables -t {table} -nL {chain} {index}')
         self.RULE = re.sub(r'\s+', ' ', r.stdout.decode().strip())  # type: ignore
-
-
-def run(cmd: str):
-    r = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    if r.returncode != 0 or r.stderr:
-        print(f'[red]{r.stderr.decode()}', file=sys.stderr)
-        sys.exit(r.returncode)
-    return r
-
-
-def clear_traces(signum: Union[int, None] = None, frame: Union[FrameType, None] = None):
-    for chain in ['PREROUTING', 'OUTPUT']:
-        run(
-            f'iptables -t raw -L {chain} --line-numbers | grep TRACE | awk \'{{ print $1 }}\' | sort -r | xargs -I {{}} iptables -t raw -D {chain} {{}}'
-        )
-    if signum:
-        sys.exit()
-
-
-def add_traces(match_rule: str):
-    for chain in ['PREROUTING', 'OUTPUT']:
-        run(f'iptables -t raw -I {chain} 1 {match_rule} -j TRACE')
 
 
 def parse_log(log: str):
@@ -66,7 +66,7 @@ def parse_log(log: str):
 
 
 def poll_then_log():
-    run('dmesg -C')
+    run_cmd('dmesg -C')
     proc = subprocess.Popen(['dmesg', '-w'], stdout=subprocess.PIPE)
     assert proc.stdout
     while True:
@@ -82,11 +82,7 @@ def main(
         str, Argument(help='Will be passed to iptables to match packets.')
     ]
 ):
+    signal(SIGINT, clear_traces)
     clear_traces()
     add_traces(match_rule)
     poll_then_log()
-
-
-if __name__ == '__main__':
-    signal(SIGINT, clear_traces)
-    app(prog_name='iptdump')
